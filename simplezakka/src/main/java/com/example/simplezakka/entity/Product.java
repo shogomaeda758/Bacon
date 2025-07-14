170883808593878dc8bea7867a8427058bbeb9eb
sohei.harada <sohei.harada@avantcorp.com>
2025-07-14 13:45:39 +0900

    SB起動完了

--- simplezakka/src/main/java/com/example/simplezakka/entity/Product.java
+++ simplezakka/src/main/java/com/example/simplezakka/entity/Product.java
@@ -51,8 +51,4 @@ public class Product {
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
-    public void setPrice(BigDecimal price2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPrice'");
-    }
 }
