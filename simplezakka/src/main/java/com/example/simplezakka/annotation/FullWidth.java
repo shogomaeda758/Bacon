dbeffb787daae0fc65e77ff59db0da6c3c1de327
Risa.Suzuki <risa.suzuki@avantcorp.com>
2025-07-25 14:50:19 +0900

    全角半角

---
+++ simplezakka/src/main/java/com/example/simplezakka/annotation/FullWidth.java
@@ -0,0 +1,15 @@
+package com.example.simplezakka.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FullWidthValidator.class)
+@Documented
+public @interface FullWidth {
+    String message() default "全角文字で入力してください。";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ simplezakka/src/main/java/com/example/simplezakka/annotation/HalfWidth.java
@@ -0,0 +1,15 @@
+package com.example.simplezakka.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = HalfWidthValidator.class)
+@Documented
+public @interface HalfWidth {
+    String message() default "半角文字で入力してください。";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
