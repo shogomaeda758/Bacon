5fb6625eb20b130529db24897a00bc787e9c0d16
Risa.Suzuki <risa.suzuki@avantcorp.com>
2025-07-25 15:17:52 +0900

    order追加

--- simplezakka/src/main/java/com/example/simplezakka/dto/order/CustomerInfo.java
+++ simplezakka/src/main/java/com/example/simplezakka/dto/order/CustomerInfo.java
@@ -1,5 +1,7 @@
 package com.example.simplezakka.dto.order;
 
+import com.example.simplezakka.annotation.FullWidth;
+import com.example.simplezakka.annotation.HalfWidth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,18 +19,22 @@ public class CustomerInfo {
 
     @NotBlank(message = "氏名は必須です。")
     @Size(max = 100, message = "氏名は100文字以内で入力してください。")
+    @FullWidth(message = "氏名は全角文字で入力してください。") // 氏名: 全角指定
     private String name; 
 
     @NotBlank(message = "メールアドレスは必須です。")
     @Email(message = "有効なメールアドレス形式で入力してください。")
     @Size(max = 255, message = "メールアドレスは255文字以内で入力してください。")
+    @HalfWidth(message = "メールアドレスは半角文字で入力してください。") // メールアドレス: 半角指定
     private String email;
 
     @NotBlank(message = "住所は必須です。")
     @Size(max = 500, message = "住所は500文字以内で入力してください。")
+    @FullWidth(message = "住所は全角文字で入力してください。") // 住所: 全角指定
     private String address;
 
     @NotBlank(message = "電話番号は必須です。")
     @Pattern(regexp = "^[0-9]{10,11}$", message = "電話番号は10桁または11桁の数字で入力してください。")
+    @HalfWidth(message = "電話番号は半角数字で入力してください。") // 電話番号: 半角指定
     private String phoneNumber;
 }
